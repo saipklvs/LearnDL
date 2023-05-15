0eafe795a434bfd4ade1309f72bcc3d31747df15
vlottala <pavanlottala94@gmail.com>
2023-05-14 21:43:11 -0500

    Added how to plot the graph

--- copilot_test.py
+++ copilot_test.py
@@ -20,3 +20,22 @@ url = "https://download.pytorch.org/torchaudio/tutorial-assets/steam-train-whist
 with requests.get(url, stream=True) as response:
     metadata = torchaudio.info(response.raw)
 print(metadata)
+
+### Loading audio files
+waveform, sample_rate = torchaudio.load(SAMPLE_WAV)
+
+def plot_waveform(waveform, sample_rate):
+    waveform = waveform.numpy()
+    num_channels, num_frames = waveform.shape
+    time_axis = torch.arange(0, num_frames) / sample_rate
+    
+    figure, axes = plt.subplots(num_channels, 1)
+    if num_channels == 1:
+        axes = [axes]
+    for c in range(num_channels):
+        axes[c].plot(time_axis, waveform[c], linewidth=1)   
+        axes[c].grid(True)
+        if num_channels > 1:
+            axes[c].set_ylabel(f'Channel {c+1}') 
+    figure.suptitle('Waveform')
+    plt.show(block=False)
