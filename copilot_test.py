2d502d407392f1b33fedb0de356da851ff18116d
vlottala <pavanlottala94@gmail.com>
2023-05-14 21:38:29 -0500

    Added test

--- copilot_test.py
+++ copilot_test.py
@@ -3,3 +3,20 @@ import torch
 import torchaudio
 import io 
 import boto3
+import matplotlib.pyplot as plt
+
+import requests
+from botocore import UNSIGNED
+from botocore.config import Config
+from torchaudio.utils import download_asset
+from IPython.display import Audio
+
+### Downloading the audio files
+SAMPLE_GSM = download_asset("tutorial-assets/steam-train-whistle-daniel_simon.gsm")
+SAMPLE_WAV = download_asset("tutorial-assets/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042.wav")
+SAMPLE_WAV_8000 = download_asset("tutorial-assets/Lab41-SRI-VOiCES-src-sp0307-ch127535-sg0042-8000hz.wav")
+
+url = "https://download.pytorch.org/torchaudio/tutorial-assets/steam-train-whistle-daniel_simon.wav"
+with requests.get(url, stream=True) as response:
+    metadata = torchaudio.info(response.raw)
+print(metadata)
